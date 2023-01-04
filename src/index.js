1fbfba00ba1985c0b40acaf7314bccc1bab18680
nk965 <nicholaskwok3@gmail.com>
2023-01-04 23:35:32 +0800

    auth, login deprecated

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import "assets/styles/tailwind.css";
 // layouts
 
 import Admin from "layouts/Admin.js";
-import Auth from "layouts/Auth.js";
-
 // views without layouts
 
 import Landing from "views/Landing.js";
@@ -19,7 +17,6 @@ ReactDOM.render(
     <Switch>
       {/* add routes with layouts */}
       <Route path="/admin" component={Admin} />
-      <Route path="/auth" component={Auth} />
       {/* add routes without layouts */}
       <Route path="/landing" exact component={Landing} />
       <Route path="/" exact component={Landing} />
