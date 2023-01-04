2944a305cea2a7b5f6fe55dc1397b1026f8d9786
nk965 <nicholaskwok3@gmail.com>
2023-01-04 23:45:01 +0800

    remove deprecated components

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -13,7 +13,6 @@ import FooterAdmin from "components/Footers/FooterAdmin.js";
 import Dashboard from "views/admin/Dashboard.js";
 import Maps from "views/admin/Maps.js";
 import Settings from "views/admin/Settings.js";
-import Tables from "views/admin/Tables.js";
 
 export default function Admin() {
   return (
@@ -28,7 +27,6 @@ export default function Admin() {
             <Route path="/admin/dashboard" exact component={Dashboard} />
             <Route path="/admin/maps" exact component={Maps} />
             <Route path="/admin/settings" exact component={Settings} />
-            <Route path="/admin/tables" exact component={Tables} />
             <Redirect from="/admin" to="/admin/dashboard" />
           </Switch>
           <FooterAdmin />
