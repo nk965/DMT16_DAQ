8113a0c7917513dd2f08439ac31288017b00adfd
nk965 <nicholaskwok3@gmail.com>
2023-01-04 17:19:44 +0000

    rename authnavbar to landingnavbar

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // components
 
-import Navbar from "components/Navbars/AuthNavbar.js";
+import Navbar from "components/Navbars/LandingNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 export default function Landing() {
