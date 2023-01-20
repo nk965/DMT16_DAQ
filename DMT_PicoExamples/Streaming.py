e29884ff998fa01f7524a9bb0644a7b1755a3105
nk965 <nicholaskwok3@gmail.com>
2023-01-20 18:42:50 +0000

    CJC channel type

--- DMT_PicoExamples/Streaming.py
+++ DMT_PicoExamples/Streaming.py
@@ -24,7 +24,7 @@ def record_data(recording_period):
 
     # set CJC channel 
 
-    status["set_channel"] = tc08.usb_tc08_set_channel(chandle, 0,'C')
+    status["set_channel"] = tc08.usb_tc08_set_channel(chandle, 0,'K')
     assert_pico2000_ok(status["set_channel"])
 
     # set all channels from TC08_config file
