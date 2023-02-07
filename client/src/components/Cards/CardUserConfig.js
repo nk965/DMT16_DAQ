432d1d98d69e3a246803f15b3d03633ef3b3720c
nk965 <nicholaskwok3@gmail.com>
2023-02-07 20:50:53 +0000

    format using prettier

--- client/src/components/Cards/CardUserConfig.js
+++ client/src/components/Cards/CardUserConfig.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function CardUserConfig({postResult}) {
-
+export default function CardUserConfig({ postResult }) {
   const [userInput, setUserInput] = useState({
-    // default values 
+    // default values
     syrLen: 150,
     syrDia: 30,
     dyeSpeed: 8,
@@ -13,25 +12,23 @@ export default function CardUserConfig({postResult}) {
     lenExperiment: 250,
     PIVfreq: 300,
     Datafreq: 5,
-
   });
 
   const handleChange = (event) => {
-    const { name, type, value } = event.target
+    const { name, type, value } = event.target;
 
     switch (type) {
-      case 'Number': 
+      case "Number":
         setUserInput({ ...userInput, [name]: Number(value) });
         break;
       default:
         setUserInput({ ...userInput, [name]: value });
     }
-  
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    postResult(userInput); // TODO: instead of console logging, change func to POST 
+    postResult(userInput); // TODO: instead of console logging, change func to POST
   };
 
   return (
@@ -39,7 +36,9 @@ export default function CardUserConfig({postResult}) {
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
         <div className="rounded-t bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
-            <h6 className="text-blueGray-700 text-xl font-bold">User Configuration</h6>
+            <h6 className="text-blueGray-700 text-xl font-bold">
+              User Configuration
+            </h6>
             <button
               className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
@@ -50,6 +49,7 @@ export default function CardUserConfig({postResult}) {
         </div>
         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
           <form onSubmit={handleSubmit}>
+            <button type="submit">Submit</button>
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               Dye Injection Characteristics
             </h6>
@@ -83,7 +83,7 @@ export default function CardUserConfig({postResult}) {
                     type="number"
                     name="syrDia"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    value = {userInput.syrDia}
+                    value={userInput.syrDia}
                     onChange={handleChange}
                   />
                 </div>
@@ -114,11 +114,11 @@ export default function CardUserConfig({postResult}) {
                   >
                     Enable Pulse Mode?
                   </label>
-                  <select 
-                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" 
-                  name="enPulse" 
-                  value={userInput.enPulse}
-                  onChange={handleChange}
+                  <select
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    name="enPulse"
+                    value={userInput.enPulse}
+                    onChange={handleChange}
                   >
                     <option value={"True"}>Yes</option>
                     <option value={"False"}>No</option>
@@ -208,10 +208,10 @@ export default function CardUserConfig({postResult}) {
               </div>
             </div>
             <hr className="mt-6 border-b-1 border-blueGray-300" />
-            <div>
-              <button>Submit Contact</button>
-            </div>
           </form>
+          <div>
+              <button type="submit">Submit Contact</button>
+          </div>
         </div>
       </div>
     </>
