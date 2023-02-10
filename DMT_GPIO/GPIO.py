3fa1336e37026b432000ef78c2a830f1f4bffc31
nk965 <nicholaskwok3@gmail.com>
2023-02-10 18:33:54 +0000

    refactor code

--- DMT_GPIO/GPIO.py
+++ DMT_GPIO/GPIO.py
@@ -4,53 +4,106 @@ from datetime import datetime, date
 import time
 import numpy as np
 import csv
-import pigpio # Library for high speed gpio
+import pigpio  # Library for high speed gpio
 
-# On Startup: $ sudo pigpiod
-# Go to Directory
-# To run: ~/Directory $ python3 GPIO.py 12 16
-    # Numbers represent the pins being checked (leaving blank will check all pins)
+"""
+@author: Jimmy van de Worp
+TODO
 
+"""
 
-last = [None]*32
-cb = [] # Initialising list for data
-py_data = [['GPIO','State','Tick','Diff']] # Titles
+'''
 
-# Defining Callback Function, GPIO = Pin No., level = state (1=HIGH, 0=LOW), tick = time (in us) since RPi bootup
-def cbf(GPIO, level, tick):
-   if last[GPIO] is not None:
-      diff = pigpio.tickDiff(last[GPIO], tick) # Time difference (in us) between the current event change and the last change
-      py_data.append([GPIO, level, tick, diff])
-      #print("G={} l={} t={} d={}".format(GPIO, level, tick, diff)) - Debug Printing - IGNORE
-   last[GPIO] = tick # Resetting the new previous GPIO state and tick time
+Current start-up procedure:
+$ sudo pigpiod 
+Go to Directory
 
-pi = pigpio.pi() # Connects to Local Pi
+Running procedure:
 
-if not pi.connected: # Exits code if no connection
-   exit()
+To run: ~/Directory $ python3 GPIO.py 12 16
+Numbers represent the pins being checked (leaving blank will check all pins)
+Defining Callback Function, GPIO = Pin No., level = state (1=HIGH, 0=LOW), tick = time (in us) since RPi bootup
 
-if len(sys.argv) == 1:
-   G = range(0, 32) # Total of 32 Pins
-else:
-   G = []
-   for a in sys.argv[1:]:
-      G.append(int(a)) # For running code on multiple systems - IGNORE
+'''
+
+def procedure(GPIO, level, tick):
+
+   '''TODO: Write description
+
+    Returns:
+        None 
    
-for g in G:
-   cb.append(pi.callback(g, pigpio.EITHER_EDGE, cbf)) # Calls function once event change is detected on pin g
+   '''
 
-# Closing Procedure
-try:
-   while True:
-      time.sleep(60)
-except KeyboardInterrupt:
-   print("\nTidying up")
-   with open('time_GPIO' + str(date.today()) + '.csv', 'a+', newline='') as csvfile: # Writing Data into .csv
-        writer = csv.writer(csvfile)
-        for i in py_data:
-            row = [i]
-            writer.writerow(row) # Writing one row at a time
-   for c in cb:
-      c.cancel()
+   if last[GPIO] is not None:
+   
+      # Time difference (in us) between the current event change and the last change
+   
+      diff = pigpio.tickDiff(last[GPIO], tick)
+   
+      py_data.append([GPIO, level, tick, diff])
+   
+      # print("G={} l={} t={} d={}".format(GPIO, level, tick, diff)) - Debug Printing - IGNORE
+   
+   last[GPIO] = tick  # Resetting the new previous GPIO state and tick time
 
-pi.stop()
+
+if __name__ == '__main__':
+
+   last = [None]*32
+
+   cb = []  # Initialising list for data
+   
+   py_data = [['GPIO', 'State', 'Tick', 'Diff']]  # Titles
+
+   pi = pigpio.pi()  # Connects to Local Pi
+
+   if not pi.connected:  # Exits code if no connection
+     
+      exit()
+
+   if len(sys.argv) == 1:
+   
+      pins = range(0, 32)  # Total of 32 Pins
+   
+   else:
+   
+      pins = []
+   
+      for i in sys.argv[1:]:
+   
+         pins.append(int(i))  # For running code on multiple systems - IGNORE
+
+   for pin in pins:
+   
+      # Calls function once event change is detected on pin g
+   
+      cb.append(pi.callback(pin, pigpio.EITHER_EDGE, procedure))
+
+   # Closing Procedure
+
+   try:
+
+      while True:
+
+         time.sleep(60)
+
+   except KeyboardInterrupt:
+
+      print("\nTidying up")
+
+      with open('time_GPIO' + str(date.today()) + '.csv', 'a+', newline='') as csvfile:  # Writing Data into .csv
+
+         writer = csv.writer(csvfile)
+
+         for i in py_data:
+
+               row = [i]
+
+               writer.writerow(row)  # Writing one row at a time
+
+      for c in cb:
+
+         c.cancel()
+
+   pi.stop()
