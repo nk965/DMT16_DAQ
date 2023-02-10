5117e5317f32848bebaa026186cf5e6afd96ffe4
jimmyvdw <jv20@ic.ac.uk>
2023-02-10 10:46:16 +0000

    Storage Data to Print at End V1

--- DMT_GPIO/GPIO.py
+++ DMT_GPIO/GPIO.py
@@ -6,17 +6,17 @@ import pigpio # Library for high speed gpio
 
 last = [None]*32
 cb = [] # Initialising list for data
+py_data = [['GPIO','State','Tick','Diff']]
 
 # Defining Storage Function to append each generated list
-def storage(datalist):
-   print(datalist)
+#def storage(datalist):
+#   print(datalist)
 
 # Defining Callback Function, GPIO = Pin No., level = state (1=HIGH, 0=LOW), tick = time (in us) since RPi bootup
 def cbf(GPIO, level, tick):
    if last[GPIO] is not None:
       diff = pigpio.tickDiff(last[GPIO], tick) # Time difference (in us) between the current event change and the last change
       datalist = [GPIO, level, tick, diff]
-      storage(datalist)
       #print("G={} l={} t={} d={}".format(GPIO, level, tick, diff))
    last[GPIO] = tick # Resetting the new previous GPIO state and tick time
 
@@ -34,6 +34,7 @@ else:
    
 for g in G:
    cb.append(pi.callback(g, pigpio.EITHER_EDGE, cbf)) # Calls function once event change is detected on pin g
+   py_data.append(cb)
 
 # Closing Procedure
 try:
@@ -41,6 +42,8 @@ try:
       time.sleep(60)
 except KeyboardInterrupt:
    print("\nTidying up")
+   np_data = np.array(py_data)
+   print(np_data)
    for c in cb:
       c.cancel()
 
