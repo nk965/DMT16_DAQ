0494ba606d466ed359e2a478d4a729298a3718d3
nk965 <nicholaskwok3@gmail.com>
2023-02-17 16:19:21 +0000

    convert base 15 to sampling interval

--- DMT_RPi/Serial.py
+++ DMT_RPi/Serial.py
@@ -24,13 +24,28 @@ def convert_back_base_15(UART_output):
 
     string = str(UART_output.hex()) # Convert back to hex string
 
-    for i in range(len(string)): # Loop through the string input
-        if i%2 == 1: # Every 2 elements, it has been padded by +1 so we remove the padding then de-convert
-            ans += (int(bases[string[i]]) - 1) * (pow(15, (len(string) - 1 - i)))
-        else: # Otherwise, standard conversion
-            ans += int(bases[string[i]])*(pow(15,(len(string)-1-i)))
+    hex_identifier = string[:2]
 
-    return ans
+    sampling_interval_hex = string[2:]
+
+    for i in range(len(sampling_interval_hex)): # Loop through the string input
+        if i%2 == 1: # Every 2 elements, it has been padded by +1 so we remove the padding then de-convert
+            ans += (int(bases[sampling_interval_hex[i]]) - 1) * (pow(15, (len(sampling_interval_hex) - 1 - i)))
+        else: # Otherwise, standard conversion
+            ans += int(bases[sampling_interval_hex[i]])*(pow(15,(len(sampling_interval_hex)-1-i)))
+
+    message = {
+        "hex_identifier": hex_identifier,
+        "sampling_interval_hex": sampling_interval_hex,
+        "sampling_interval_ms": convert_to_ms(sampling_interval_hex)
+    }
+
+    return message
+
+def convert_to_ms(sampling_interval_hex):
+
+    actual = (((sampling_interval_hex - 0) * (5000 - 100)) /
+              ((15**(8 // 4) - 1) - 0)) + 0
 
 
 if __name__ == '__main__':
