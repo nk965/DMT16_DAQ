24b1edd1a0c10593909588988e99f496ee2c3ea4
Pike Amornchat <pa620@ic.ac.uk>
2023-02-24 14:45:07 +0000

    change testbed_mega

--- DMT_Arduino/TB_microcontroller/TestBed_Mega/TestBed_Mega.ino
+++ DMT_Arduino/TB_microcontroller/TestBed_Mega/TestBed_Mega.ino
@@ -2,16 +2,17 @@ int incomingByte;
 
 void setup() {
   Serial.begin(230400);
-  Serial1.begin(230400);
+  // Serial1.begin(230400);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
     // read the incoming byte:
     incomingByte = Serial.read();
-
     // say what you got:
     Serial.write(incomingByte);
-    Serial1.write(incomingByte);
+    // Serial1.write(incomingByte);
   }
 }
