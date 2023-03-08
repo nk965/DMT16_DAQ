dd9b29e30c72394e9e9ec61d014cb18525ebca15
jimmyvdw <jv20@ic.ac.uk>
2023-03-08 14:50:18 +0000

    launcher.sh bash file

---
+++ DMT_RPi/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, run pigpiod library, then to this directory, then execute python scripts, then back home
+
+cd /
+sudo pigpiod
+cd home/icl-dmt16/DMT16_DAQ/DMT_RPi
+python3 Serial.py
+python3 Streaming.py & python3 GPIO.py 12 16 20
+cd /
+sudo killall pigpiod
